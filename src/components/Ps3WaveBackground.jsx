7c06204f9ae909b4ddfe6d9bedfcfe1e6bb8f8bb
Alex Seidler <ajseidler0526@gmail.com>
2026-05-24 16:51:39 -0500

    Fix release asset packaging

--- src/components/Ps3WaveBackground.jsx
+++ src/components/Ps3WaveBackground.jsx
@@ -1,10 +1,9 @@
 import "./Ps3WaveBackground.css";
-import ps3WaveVideo from "../../xmb-on-web-main/images/vid.mp4";
 
 export default function Ps3WaveBackground() {
   return (
     <div className="ps3-wave-background" aria-hidden="true">
-      <video className="ps3-wave-video" src={ps3WaveVideo} autoPlay muted loop playsInline />
+      <video className="ps3-wave-video" src="/ps3-wave-background.mp4" autoPlay muted loop playsInline />
     </div>
   );
 }
